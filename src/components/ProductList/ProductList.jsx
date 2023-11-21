3222659b63f7d90decb6d4c67b26e60f3708927c
Vyacheslav <slava.shilin07@mail.ru>
2023-11-21 14:48:56 +0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://192.168.0.78:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -71,11 +71,7 @@ const ProductList = () => {
     return (
         <div className={'list'}>
             {products.map(item => (
-                <ProductItem
-                    product={item}
-                    onAdd={onAdd}
-                    className={'item'}
-                />
+                <ProductItem product={item} onAdd={onAdd} className={'item'} key={item.id}/>
             ))}
         </div>
     );
