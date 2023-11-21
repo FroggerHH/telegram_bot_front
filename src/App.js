b75b30813648dda37871d862df9f0240ee78d43d
Vyacheslav <slava.shilin07@mail.ru>
2023-11-21 16:14:02 +0300

    Made <ProductList/> no Route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 
 function App() {
@@ -18,9 +17,7 @@ function App() {
             <p>Hello World 1</p>
             <p>Hello World 2</p>
             <Header/>
-            <Routes>
-                <Route index element={<ProductList/>}/>
-            </Routes>
+            <ProductList/>
         </div>
     );
 }
