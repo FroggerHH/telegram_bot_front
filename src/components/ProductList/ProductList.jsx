92be4a5f4c40b82c203e327faa98fb0692373997
Vyacheslav <slava.shilin07@mail.ru>
2023-11-22 14:11:20 +0300

    -

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,7 +2,7 @@ import React, {useCallback, useEffect, useState} from 'react';
 import './ProductList.css';
 import {useTelegram} from "../../hooks/useTelegram";
 import ProductItem from "../ProductItem/ProductItem";
-
+import unirest from "unirest";
 
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
@@ -11,87 +11,99 @@ const getTotalPrice = (items = []) => {
 }
 
 const ProductList = () => {
-    const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
+        const [addedItems, setAddedItems] = useState([]);
+        const {tg, queryId} = useTelegram();
 
-    let products = []
-    try {
-        fetch('http://5.35.9.71:8000/api/get-products', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then(r => {
-            console.log(`Get products response: ${r}`)
-            products = r.json()
-        })
-    } catch (e) {
-        console.log(e)
-    }
-
-    const onSendData = useCallback(() => {
+        let products = []
         try {
-            const data = {
-                products: addedItems,
-                totalPrice: getTotalPrice(addedItems),
-                queryId,
-            }
-            fetch('http://5.35.9.71:8000/api/web-data', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            }).then(r => () => {
-                console.log(`Web data response: ${r}`)
+                const req = unirest.request("GET", 'http://5.35.9.71:8000/api/get-products', {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json'
+            });
+            req.end(function (res) {
+                if (res.error) throw new Error(res.error);
+                console.log(`Unirest response: body:'${res.body}', status: '${res.status}', json: '${res.json()}'`);
+                products = res.json()
             })
-        } catch (e) {
+
+            // fetch('http://5.35.9.71:8000/api/get-products', {
+            //     method: 'GET',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     }
+            // }).then(r => {
+            //     console.log(`Get products response: ${r}`)
+            //     products = r.json()
+            // })
+        } catch
+            (e) {
             console.log(e)
         }
-    }, [addedItems, queryId])
 
-
-    useEffect(() => {
+        const onSendData = useCallback(() => {
             try {
-                tg.onEvent('mainButtonClicked', onSendData)
-                tg.offEvent('mainButtonClicked', onSendData)
-            } catch
-                (e) {
+                const data = {
+                    products: addedItems,
+                    totalPrice: getTotalPrice(addedItems),
+                    queryId,
+                }
+                fetch('http://5.35.9.71:8000/api/web-data', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data)
+                }).then(r => () => {
+                    console.log(`Web data response: ${r}`)
+                })
+            } catch (e) {
                 console.log(e)
             }
-        }, [onSendData, tg]
-    )
+        }, [addedItems, queryId])
 
 
-    const onAdd = (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
-        let newItems = [];
+        useEffect(() => {
+                try {
+                    tg.onEvent('mainButtonClicked', onSendData)
+                    tg.offEvent('mainButtonClicked', onSendData)
+                } catch
+                    (e) {
+                    console.log(e)
+                }
+            }, [onSendData, tg]
+        )
 
-        if (alreadyAdded) {
-            newItems = addedItems.filter(item => item.id !== product.id);
-        } else {
-            newItems = [...addedItems, product];
+
+        const onAdd = (product) => {
+            const alreadyAdded = addedItems.find(item => item.id === product.id);
+            let newItems = [];
+
+            if (alreadyAdded) {
+                newItems = addedItems.filter(item => item.id !== product.id);
+            } else {
+                newItems = [...addedItems, product];
+            }
+
+            setAddedItems(newItems)
+
+            if (newItems.length === 0) {
+                tg.MainButton.hide();
+            } else {
+                tg.MainButton.show();
+                tg.MainButton.setParams({
+                    text: `Купить ${getTotalPrice(newItems)}`
+                })
+            }
         }
 
-        setAddedItems(newItems)
-
-        if (newItems.length === 0) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-            tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
-            })
-        }
+        return (
+            <div className={'list'}>
+                {products.map(item => (
+                    <ProductItem product={item} onAdd={onAdd} className={'item'} key={item.id}/>
+                ))}
+            </div>
+        );
     }
-
-    return (
-        <div className={'list'}>
-            {products.map(item => (
-                <ProductItem product={item} onAdd={onAdd} className={'item'} key={item.id}/>
-            ))}
-        </div>
-    );
-};
+;
 
 export default ProductList;
