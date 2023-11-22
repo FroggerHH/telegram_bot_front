99bce41e829bbc6eff690e1cf8a3ec1ebeb0ed8b
Vyacheslav <slava.shilin07@mail.ru>
2023-11-22 12:14:13 +0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,11 +1,90 @@
+import React, {useCallback, useEffect, useState} from 'react';
 import './ProductList.css';
+import {useTelegram} from "../../hooks/useTelegram";
 import ProductItem from "../ProductItem/ProductItem";
 
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        try {
+            const data = {
+                products: addedItems,
+                totalPrice: getTotalPrice(addedItems),
+                queryId,
+            }
+            fetch('http://5.35.9.71:8000/api/web-data', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            }).then(r => () => {
+                console.log(`Response: ${r}`)
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }, [addedItems, queryId])
+
+
+    useEffect(() => {
+            try {
+                tg.onEvent('mainButtonClicked', onSendData)
+                tg.offEvent('mainButtonClicked', onSendData)
+            } catch
+                (e) {
+                console.log(e)
+            }
+        }, [onSendData, tg]
+    )
+
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
         <div className={'list'}>
-            <ProductItem product={"Product"}/>
+            {products.map(item => (
+                <ProductItem product={item} onAdd={onAdd} className={'item'} key={item.id}/>
+            ))}
         </div>
     );
 };
