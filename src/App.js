cf9b84c7abc95a6606ff917f65e369a383acc772
Vyacheslav <slava.shilin07@mail.ru>
2023-11-26 14:49:54 +0300

    =

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import './App.css';
-import React, {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+import React from "react";
 import Header from "./components/Header/Header";
 import ProductList from "./components/ProductList/ProductList";
+import {Form, Route, Routes} from "react-router-dom";
 
 function App() {
-    const {tg, user} = useTelegram();
-    if (user === undefined || user === null || tg === undefined || tg === null) {
-        console.log(`Telegram object is not defined`)
-
-        document.documentElement.style.setProperty('--tg-theme-bg-color', '#17212b');
-        document.documentElement.style.setProperty('--tg-theme-secondary-bg-color', '#232e3c');
-        document.documentElement.style.setProperty('--tg-theme-text-color', '#e9e9e9');
-        document.documentElement.style.setProperty('--tg-theme-button-color', '#5ac7f9');
-        document.documentElement.style.setProperty('--tg-theme-button-text-color', '#e8f0fa');
-        document.documentElement.style.setProperty('--tg-theme-hint-color', '#747575');
-    }
-
-    useEffect(() => {
-        tg.ready();
-        tg.expand();
-    }, [tg])
+    document.documentElement.style.setProperty('--bg-color', '#17212b');
+    document.documentElement.style.setProperty('--secondary-bg-color', '#232e3c');
+    document.documentElement.style.setProperty('--text-color', '#e9e9e9');
+    document.documentElement.style.setProperty('--button-color', '#5ac7f9');
+    document.documentElement.style.setProperty('--button-text-color', '#e8f0fa');
+    document.documentElement.style.setProperty('--hint-color', '#747575');
 
     return (
         <div className="App">
             <Header/>
-            <ProductList/>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
         </div>
     );
 }
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,11 +1,9 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
-import {useTelegram} from "../../hooks/useTelegram";
 import ProductItem from "../ProductItem/ProductItem";
 import getProducts from "../../api";
 
 const ProductList = () => {
-        const {tg} = useTelegram();
         const [products, setProducts] = useState([]);
         if (products.length === 0)
             try {
@@ -13,26 +11,6 @@ const ProductList = () => {
             } catch (e) {
                 console.log(e)
             }
-
-        const onSendData = useCallback(() => {
-            try {
-            } catch (e) {
-                console.log(e)
-            }
-        }, [])
-
-
-        useEffect(() => {
-                try {
-                    tg.onEvent('mainButtonClicked', onSendData)
-                    tg.offEvent('mainButtonClicked', onSendData)
-                } catch
-                    (e) {
-                    console.log(e)
-                }
-            },
-            [onSendData, tg]
-        )
 
         return (
             <div className={'list'}>
