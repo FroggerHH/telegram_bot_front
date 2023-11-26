73fcd51e4d8dd0689abde411542130214591ac6b
Vyacheslav <slava.shilin07@mail.ru>
2023-11-26 14:58:10 +0300

    sdf

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Button from "../Button/Button";
 
 const Header = () => {
     return (
         <div className={'header'}>
+            <Button onClick={() => window.location.href = '/'}> Гдавная </Button>
+            <Button onClick={() => window.location.href = '/form'}> Форма </Button>
             <span className={'username'}>
                 username
             </span>
